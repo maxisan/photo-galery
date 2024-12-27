b3c5985e3863701b81c310d698d8ca5ab125ce9f
Maximiliano Sanchez <maxisanchez19@gmail.com>
2024-12-27 14:33:39 +0000

    re-build

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
 import Landing from "./features/landing";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 export default function Index() {
+  GoogleSignin.configure();
+
   return (
     <Landing />
   );
